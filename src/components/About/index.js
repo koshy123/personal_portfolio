c9bea2213af85f06b186df3e3fdf4522408b6d2d
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-10 21:17:41 -0500

    add styling to about para

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,10 +10,11 @@ const About = () => {
             </h1>
 
             <p>
+                I amm ambitious, curious and excited for new adventures. 
 
             </p>
             <p>
-
+            Software sticks out to me because of the power it has to change many peoples lives. It can help companies run more efficent or give an individual tools to make their day to day life easier.
             </p>
             <p>
 
