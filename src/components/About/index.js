850563a7e33e7cd642e9546faea69b17d305cfea
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-13 15:29:13 -0500

    add more to abuut section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,23 +1,25 @@
 import './index.scss'
+import self from '../../assets/images/self.jpg'
 
 const About = () => {
      
     return(
         <div className='contrainer about-page'>
             <div className='text-zone'>
+                <img className="self" src={self} ></img>
             <h1>
                 About
             </h1>
 
-            <p>
-                I amm ambitious, curious and excited for new adventures. 
+            <p className="about">
+                I am ambitious, curious and excited for new adventures. 
 
             </p>
-            <p>
-            Software sticks out to me because of the power it has to change many peoples lives. It can help companies run more efficent or give an individual tools to make their day to day life easier.
+            <p className="about">
+            Software stuck out to me because of the power it has to change many lives. It can help companies run more efficent or give an individual tools to make their day to daily life easier.
             </p>
-            <p>
-
+            <p className="about">
+            With the toolbelt I have, I plan to give my all to any project I work on.
             </p>
             </div>
 
