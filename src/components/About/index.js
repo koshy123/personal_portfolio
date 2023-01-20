268a84f81d4e33e617bd3d5cccf6c44431fdb580
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-20 00:29:11 -0500

    more styling added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,29 +11,33 @@ const About = () => {
                 <h1 className='title'>
                     About
                 </h1>
-                <img className="self" src={self} ></img>
+                <img  src={self} ></img>
                 <p className="about">
-                    I am ambitious, curious and excited for new adventures.
+                    I am curious and excited for new challenges and opportunities.
 
                 </p>
                 <p className="about">
-                    Software stuck out to me because of the power it has to change many lives. It can help companies run more efficent or give an individual tools to make their day to daily life easier.
+                    With software development I can help companies or individuals run more efficiently.
                 </p>
                 <p className="about">
-                    With the toolbelt I have, I plan to give my all to any project I work on.
+                    My family, friends and faith are close to my heart. 
                 </p>
-                <div className='skills'>
+                <p className="about">
+                Some other topics that interest me are Sports, History and Economics. 
+                </p>
+
+                <div >
                     <h1 className='title'>Skills</h1>
                     <div className='skill_layout'>
-                        <DevIcon className='skill_image'   icon="react"></DevIcon>
-                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
-                        <img  className='skill_image'   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
-                        <img  className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
-                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
-                        <img  className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
-                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
-                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
-                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                        <DevIcon className='skill_image' icon="react"></DevIcon>
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
+                        <img className='skill_image' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
 
                     </div>
 
