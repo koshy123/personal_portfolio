b04bb96372940710923c575521e1b43e541c6c94
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-19 23:13:57 -0500

    redoing style spacing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,13 @@
 import './index.scss'
 import self from '../../assets/images/self.jpg'
-import css from '../../assets/images/css.png'
-import html from '../../assets/images/html5.png'
-import javscript from '../../assets/images/javscript.jpeg'
 import DevIcon from "devicon-react-svg";
 
 const About = () => {
 
+
     return (
         <div className='about-layout' >
-            <div className='text-zone page'>
+            <div >
                 <h1 className='title'>
                     About
                 </h1>
@@ -27,15 +25,15 @@ const About = () => {
                 <div className='skills'>
                     <h1 className='title'>Skills</h1>
                     <div className='skill_layout'>
-                        <DevIcon width="10%"  icon="react"></DevIcon>
-                        <img width="10%"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
-                        <img  width="10%"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
-                        <img  width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
-                        <img width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
-                        <img  width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
-                        <img width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
-                        <img width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
-                        <img width="10%" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                        <DevIcon className='skill_image'   icon="react"></DevIcon>
+                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
+                        <img  className='skill_image'   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+                        <img  className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
+                        <img  className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" />
+                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></img>
+                        <img className='skill_image'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
 
                     </div>
 
