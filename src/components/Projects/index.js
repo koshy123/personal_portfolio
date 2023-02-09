31e5d8604ae95c46b691eeb267f187089b42a147
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-02-09 15:40:16 -0500

    change git repo for two

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -15,40 +15,36 @@ const Projects = () => {
                 <div className='name_pic'  >
                     <h1 className='projects' >Dev-Hub</h1>
                     <img className="web_shots" src={dev_hub}></img>
-                    <a target="_blank" href="https://the-dev-hub.vercel.app/"></a>                    
                     <p className='describe'>This group project utalized React.js</p>
                     <div className='buttons'>
-                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Live Link</Button>
+                    <Button className='button' target="_blank" href="https://github.com/koshy123/GameOfThronesHQ" >Github Repo</Button>
                     <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Live Link</Button>
                         </div>
                 </div>
                 <div className='name_pic'>
                     <h1 className='projects' >Sports Discords</h1>
                     <img className="web_shots" src={sports_discord}></img>
-                    <a target="_blank" href="https://project3-front-end.vercel.app/"></a>
                     <p className='describe'>This group project utalized React.js</p>
                     <div className='buttons'>
-                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Live Link</Button>
+                    <Button className='button' target="_blank" href="https://github.com/koshy123/Sports-Discord" >Github Repo</Button>
                     <Button className='button' target="_blank" href="https://project3-front-end.vercel.app/" >Live Link</Button>
                         </div>
                 </div>
                 <div className='name_pic'>
                     <h1 className='projects' >Burger App</h1>
                     <img className="web_shots" src={burger_express}></img>
-                    <a target="_blank" href="https://mern-front-qlvy1gwus-koshy123.vercel.app/burgerView"></a>
                     <p className='describe'>This group project utalized React.js</p>
                     <div className='buttons'>
-                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Live Link</Button>
+                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Github Repo</Button>
                     <Button className='button' target="_blank" href="https://mern-front-qlvy1gwus-koshy123.vercel.app/burgerView" >Live Link</Button>
                         </div>
                 </div>
                 <div className='name_pic'>
                     <h1 className='projects' >Game of Thrones</h1>
                     <img className="web_shots" src={game_thrones}></img>
-                    <a target="_blank" href="https://game-of-thrones-medp.vercel.app/components/Quote"></a>
                     <p className='describe'>This group project utalized React.js</p>
                     <div className='buttons'>
-                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Live Link</Button>
+                    <Button className='button' target="_blank" href="https://the-dev-hub.vercel.app/" >Github Repo</Button>
                     <Button className='button' target="_blank" href="https://game-of-thrones-medp.vercel.app/components/Quote" >Live Link</Button>
                     </div>
                 </div>
